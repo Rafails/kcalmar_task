57741d0de2c19bf9270f62529c8a4e22b11d6fd2
Rafails <john_qr@o2.pl>
2016-06-02 09:58:25 +0200

    something

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/rafal/Programowanie/kalmar/env"
+set -gx VIRTUAL_ENV "/home/rafal/Programowanie/moje/kcalmar_task/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
--- env/bin/django-admin.py
+++ env/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/rafal/Programowanie/kalmar/env/bin/python
+#!/home/rafal/Programowanie/moje/kcalmar_task/env/bin/python
 from django.core import management
 
 if __name__ == "__main__":
